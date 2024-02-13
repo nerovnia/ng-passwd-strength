7af838df96ba46d1fe3f26d901c236dcc629c07a
Volodymyr Nerovnia <nerv@i.ua>
2024-02-13 19:23:38 +0200

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { PasswdCheckFormComponent } from './passwd-check-form/passwd-check-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PasswdCheckFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'USENSE-Angular-Task';
+  title = 'Password Strength Checker';
 }
